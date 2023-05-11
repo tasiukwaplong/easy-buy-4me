07327debc7d2ea3b6ce315bd5d3e49451d95b3ab
Raphael Eze <eze.raph@gmail.com>
2023-05-11 12:13:54 +0100

    corrected user model

--- database/migrations/2023_04_29_152940_create_customers_table.php
+++ database/migrations/2023_04_29_152940_create_customers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
 
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
 
             $table->id();
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('users');
     }
 };
